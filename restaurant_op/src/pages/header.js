de282b8cb6fb317c3a64305d3f3495c7e2aa9b15
RodrigoZea <zearodrigo37@gmail.com>
2023-02-22 05:29:27 -0600

    banner finished

--- restaurant_op/src/pages/header.js
+++ restaurant_op/src/pages/header.js
@@ -1,6 +1,7 @@
 // Add sunny images
 import sunnyWheel from "../assets/imgs/sunny_wheel.png"
 import sunnyHead from "../assets/imgs/sunny_head.png"
+import sanji from "../assets/imgs/sanji.png"
 // style
 import '../styling/header.css'
 
@@ -10,6 +11,36 @@ const header = () => {
 
     const header = document.createElement("div");
     header.id="header";
+
+    // Header Banner
+    const bannerDiv = document.createElement("div");
+    bannerDiv.id="banner";
+    
+    const sanjiImg = document.createElement("img");
+    sanjiImg.src = sanji;
+    sanjiImg.id = "sanji-img";
+    bannerDiv.append(sanjiImg);
+
+    const titleDiv = document.createElement("div");
+    titleDiv.id = "title-container";
+    const bannerTitle = document.createElement("div");
+    bannerTitle.id = "banner-title";
+    bannerTitle.textContent="SANJI'S DINER";
+    const bannerSubtitle = document.createElement("div");
+    bannerSubtitle.id = "banner-subtitle";
+    bannerSubtitle.textContent = "the most wanted food in the grand line";
+    
+
+
+    titleDiv.append(bannerTitle);
+    titleDiv.append(bannerSubtitle);
+    bannerDiv.append(titleDiv);
+
+    header.append(bannerDiv);
+
+    // Menu and logo container
+    const menulogoContainer = document.createElement("div");
+    menulogoContainer.id="menu-logo-container";
 
     // Logo
     const imageContainer = document.createElement("div");
@@ -25,25 +56,29 @@ const header = () => {
     sunnyImage.id = "sunny-img";
     imageContainer.append(sunnyImage);
 
-    //header.append(imageContainer);
-    header.append(imageContainer);
+    menulogoContainer.append(imageContainer);
 
+    const buttonContainer = document.createElement("div");
+    buttonContainer.id = "button-container";
     // Home
     const homeButton = document.createElement("div");
     homeButton.id = "menu-button";
     homeButton.textContent = "HOME";
-    header.append(homeButton);
+    buttonContainer.append(homeButton);
     // Menu
     const menuButton = document.createElement("div");
     menuButton.id = "menu-button";
     menuButton.textContent = "MENU";
-    header.append(menuButton);
+    buttonContainer.append(menuButton);
     // About
     const aboutButton = document.createElement("div");
     aboutButton.id = "menu-button";
     aboutButton.textContent = "ABOUT";
-    header.append(aboutButton);
+    buttonContainer.append(aboutButton);
 
+    menulogoContainer.append(buttonContainer);
+
+    header.append(menulogoContainer);
     content.append(header);
 
 }
